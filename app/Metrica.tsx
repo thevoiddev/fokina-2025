4bd1937cfc8199d3d8713eebc7c47f27a7a6bc45
Aleksey Chekunov <73482583+thevoiddev@users.noreply.github.com>
2025-12-18 22:47:16 +0500

    Add Yandex Metrika tracking component

---
+++ app/Metrica.tsx
@@ -0,0 +1,18 @@
+'use client'
+ 
+import { useEffect } from 'react'
+import { usePathname, useSearchParams } from 'next/navigation'
+
+
+export default function YandexMetrika() {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+ 
+  useEffect(() => {
+    const url = `${pathname}?${searchParams}`
+    ym(XXXXXXXX, 'hit', url);
+ 
+  }, [pathname, searchParams])
+ 
+  return null
+}
